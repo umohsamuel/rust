ef12808eda36c671fec06e1eb1a5128329975e8e
Umoh Samuel Godwin <umohsg@gmail.com>
2024-12-13 18:58:45 +0100

    fibonacci

--- fibonacci_project/src/main.rs
+++ fibonacci_project/src/main.rs
@@ -5,24 +5,24 @@ fn main() {
 
     println!("Enter a number to calculate the fibonacci value: ");
 
-     io::stdin()
+    io::stdin()
         .read_line(&mut user_number)
         .expect("Failed to read line");
 
     let user_number: u32 = match user_number.trim().parse() {
-        Ok(num)=>num,
+        Ok(num) => num,
         Err(_) => {
             println!("Invalid number, egbon");
             return;
         }
     };
 
-   let final_value = fibonacci_sequence_nth_term(user_number);
+    let final_value = fibonacci_sequence_nth_term(user_number);
 
-   println!("fibonacci value for: {user_number} is: {final_value}")
+    println!("fibonacci value for: {user_number} is: {final_value}")
 }
 
-fn fibonacci_sequence_nth_term(n: u32)-> u32{
+fn fibonacci_sequence_nth_term(n: u32) -> u32 {
     if n == 0 {
         return 0;
     } else if n == 1 {
@@ -31,12 +31,10 @@ fn fibonacci_sequence_nth_term(n: u32)-> u32{
         let mut a = 0;
         let mut b = 1;
 
-            for _i in 2..=n{
-
-              b += a;
-              a = b-a;
-
-            }
+        for _i in 2..=n {
+            b += a;
+            a = b - a;
+        }
         return b;
     }
 }
